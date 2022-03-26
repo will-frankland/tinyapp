4f398783a628e01dcb8dcb0ca01fe86aab723f28
Will Frankland <50025807+will-frankland@users.noreply.github.com>
2022-03-26 14:42:02 -0400

    Updating major errors from project

--- express_server.js
+++ express_server.js
@@ -1,12 +1,12 @@
-const express = require("express");   // Import the express package. Framework to make a simpler server (http)
-const app = express();                // Instantiate an express object for us to use
-const PORT = 8080;                    // This is the default port   (stire the port value in a variable)
+const express = require("express");
+const app = express();
+const PORT = 8080;
 const cookieSession = require("cookie-session");
 const bodyParser = require("body-parser");
 const bcrypt = require("bcryptjs");
 const req = require("express/lib/request");
-
 const { getUserByEmail, urlsForUser, generateRandomString } = require('./helpers.js');
+const { use } = require("chai");
 
 
 app.use(bodyParser.urlencoded({ extended: true }));
@@ -29,10 +29,8 @@ const urlDatabase = {
   }
 };
 
-// users object for registrations
 const users = {
   aJ48lW: {
-    // specific values for user1234
     id: "aJ48lW",
     email: "1234@gmail.com",
     password: bcrypt.hashSync("password", 10)
@@ -41,7 +39,6 @@ const users = {
 
 app.get("/urls", (req, res) => {
   const userID = req.session.user_id;
-  // Only registered and logged in users can view MyURLs
   if (userID === undefined) {
     return res.send("Please login to view")
   }
@@ -51,19 +48,27 @@ app.get("/urls", (req, res) => {
   res.render("urls_index", templateVars);
 });
 
-// Only registered and logged in users can create tiny URLs
 app.get("/urls/new", (req, res) => {
+  // check this function - failing MAJOR
   const userID = req.session.user_id
   if (!userID) {
-    return res.render("login");
+    return res.redirect("/login");
   }
   const templateVars = { urls: urlDatabase, user: users[userID] }
   res.render("urls_new", templateVars);
 });
 
-
+// if user not logged in AND if a user is logged in but does not own a url with given id
+// .. should return error msg.
 app.get("/urls/:shortURL", (req, res) => {
   const userID = req.session.user_id
+  if (!userID) {
+    return res.status(404).send("No access, please login")
+  }
+  const shortURL = req.params.shortURL
+  if (urlDatabase(shortURL).userID !== userID) {
+    return res.status(404).send("You do not own this URL")
+  }
   const templateVars = { shortURL: req.params.shortURL, longURL: urlDatabase[req.params.shortURL].longURL, user: users[userID] };
   res.render("urls_show", templateVars);
 });
@@ -104,23 +109,17 @@ app.listen(PORT, () => {
 
 app.post("/urls", (req, res) => {
   const userID = req.session.user_id
-  // const templateVars = { urls: urlDatabase, user: users[userID] }
   const shortURL = generateRandomString()
   const longURL = req.body.longURL
   urlDatabase[shortURL] = { longURL, userID }
-  // console.log(req.body);  // Log the POST request body to the console
   res.redirect(`/urls/${shortURL}`);
 });
 
 app.post("/urls/:shortURL/delete", (req, res) => {
   const userID = req.session.user_id
-  // perform check to see if it is their url
-  // only perform operation if user is logged in - stops people accessing API from behind the scenes without being logged in
   if (!userID) {
     res.send("Not authorized!")
   }
-  // const templateVars = { urls: urlDatabase, user: users[userID] }
-  // console.log(req.params);
   const shortURL = req.params.shortURL;
   delete urlDatabase[shortURL];
   res.redirect(`/urls`);
@@ -128,29 +127,21 @@ app.post("/urls/:shortURL/delete", (req, res) => {
 
 app.post("/urls/:shortURL", (req, res) => {
   const userID = req.session.user_id
-  // const templateVars = { urls: urlDatabase, user: users[userID] }
-  // const templateVars = { urls: urlDatabase, username: req.cookies['username'] }
   const longURL = req.body.longURL;
   const shortURL = req.params.shortURL;
   urlDatabase[shortURL] = { longURL, userID };
   res.redirect(`/urls`);
 });
 
-// .get renders page and form render login.ejs
 app.get("/login", (req, res) => {
   const templateVars = { user: null }
   res.render("login", templateVars);
 });
 
-// for when form is submitted
 app.post("/login", (req, res) => {
   const { email, password } = req.body
-  // variable that acceses user object values
   const _users = Object.values(users)
-  // variable that uses .find to check if client email is contained within object 'users'.
   const user = getUserByEmail(email, users);
-
-  console.log(user)
   if (!user) {
     return res.status(403).send("Invalid email")
   }
@@ -165,9 +156,9 @@ app.post("/login", (req, res) => {
   req.session.user_id = user["id"];
   res.redirect(`/urls`);
 });
-
+// cookies not clearing after logout, check this
 app.post("/logout", (req, res) => {
-  req.session.user_id = null
+  req.session = null
   res.redirect(`/urls`);
 });
 
@@ -179,18 +170,13 @@ app.get("/register", (req, res) => {
 app.post("/register", (req, res) => {
   const id = generateRandomString();
   const { email, password } = req.body;
-  // if email or password field are missing, return error with reason.
   if (!email || !password) {
     return res.status(400).send("Missing field.");
   }
-  // create a variable that accesses user object values
   const _users = Object.values(users)
-  // create another var that uses .find to check...
-  // if client input email is already reg'd by iterating through all users 
   const foundEmail = _users.find(user => {
     return user.email === email;
   })
-  // if email found then display error that email already registered.
   if (foundEmail) {
     return res.status(400).send("Email already registered.");
   }
@@ -201,7 +187,10 @@ app.post("/register", (req, res) => {
 
   console.log("users", users)
 
- 
+
   req.session.user_id = id
   res.redirect("/urls")
 });
+
+
+// app.listen needed at bottom of file?
